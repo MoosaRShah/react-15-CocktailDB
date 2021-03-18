ddfe0688358c582a4781a8d08334dfe5c762c54b
Moosa Shah <moosarshah@gmail.com>
2021-03-18 14:20:49 +0000

    cocktaildb

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi,
+        doloribus rem! Provident, placeat harum. Dolore, sint dolor animi
+        quisquam iste, distinctio recusandae et velit sapiente necessitatibus
+        incidunt, rem nemo cupiditate inventore officiis. Nostrum odit veniam
+        temporibus in dignissimos, numquam consequuntur!
+      </p>
+    </section>
+  )
+}
+
+export default About
